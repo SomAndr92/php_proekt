cfdb381a020e57f11e52bca724d601487c4bfb11
SomAndr92 <nikmos92@mail.ru>
2023-12-24 13:49:44 +0300

    first commit

---
+++ awesomeBD.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Дек 21 2023 г., 21:13
+-- Версия сервера: 10.8.4-MariaDB
+-- Версия PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `awesomeBD`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `login` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pass` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `login`, `pass`) VALUES
+(1, 'qewe', '123', 'wqeqwe'),
+(8, 'yy', 'yuytu', 'yut');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Proekt\controllers;
+use Proekt\models\UserModel;
+
+class UserController{
+
+    public function reg($json){
+        echo $json['login'];
+
+            if(!empty($json['login']) and !empty($json['pass']) and !empty($json['pass'])) {
+        $um = new UserModel;
+        $res = $um->userReg($json['login'], $json['pass'], $json['name']);
+        if($res) {
+            echo json_encode($res);
+        }
+    }
+}
+    public function auth($json){
+        echo $json['login'];
+
+            if(!empty($json['login']) and !empty($json['pass']) and !empty($json['pass'])) {
+        $um = new UserModel;
+        $res = $um->userAuth($json['login'], $json['pass'], $json['name']);
+        if($res) {
+            echo json_encode($res);
+        }
+    }
+    } 
+
+}
